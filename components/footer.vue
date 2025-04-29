3156bc83ce00b2049456aaccef4494158e706ada
Brycen <brycenong321@gmail.com>
2025-04-30 01:39:48 +0800

    updated header and footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div>
-    Footer :)
-  </div>
+  <footer class="flex flex-col bg-primary w-full py-16 px-36">
+    <div class="flex w-full justify-between">
+      <div class="text-white w-xl font-medium">
+        <div class="text-[54px] leading-[73px] mb-8">Preparedness starts with you</div>
+        <ul class="grid grid-cols-2">
+          <li class="col-span-1 mb-2">
+            <NuxtLink :to="'/kit-builder'">Kit Builder</NuxtLink>
+          </li>
+          <li class="col-span-1 mb-2">
+            <NuxtLink :to="'/about'">About</NuxtLink>
+          </li>
+          <li class="col-span-1 mb-2">
+            <NuxtLink :to="'/climate-personality'">Climate Personality</NuxtLink>
+          </li>
+          <li class="col-span-1 mb-2">
+            <NuxtLink :to="'/'">Email Us</NuxtLink>
+          </li>
+        </ul>
+      </div>
+        <img class="h-[162px]" src="/assets/svg/logo-white.svg">
+    </div>
+    <div class="ms-auto mt-4 font-medium text-white">Copyright © 2024 Climate Care Kit. Climate Care Kit does not provide professional medical advice.</div>
+  </footer>
 </template>
+
+<style lang="scss">
+</style>
--- components/header.vue
+++ components/header.vue
@@ -1,9 +1,74 @@
-<template>
-  <div>
+<script setup lang="ts">
+const isMenuOpen = ref(false);
+const dropdownMenu = useTemplateRef('dropdown-menu');
 
-  </div>
+function toggleDropdown() {
+  isMenuOpen.value = !isMenuOpen.value;
+}
+
+function closeDropdown() {
+  if (isMenuOpen.value === true) {
+    isMenuOpen.value = false;
+  }
+}
+
+onClickOutside(dropdownMenu, closeDropdown);
+
+// onMounted(() => {
+//   if (import.meta.client) {
+//   }
+// })
+
+</script>
+
+<template>
+  <header class="flex px-16 w-full h-20 justify-center bg-white/10 sticky">
+    <div class="flex w-full max-w-360 justify-between items-center">
+      <NuxtLink :to="'/'">
+        <img src="/assets/svg/logo.svg">
+      </NuxtLink>
+      <nav>
+        <ul class="flex text-xl font-bold">
+          <li class="p-2 mr-4 hover:underline hover:underline-offset-8">
+            <NuxtLink :to="'/kit-builder'">Kit Builder</NuxtLink>
+          </li>
+          <li class="p-2 mr-4 hover:underline hover:underline-offset-8">
+            <NuxtLink :to="'/kit-builder'">Climate Personality</NuxtLink>
+          </li>
+          <li class="p-2 mr-4 hover:underline hover:underline-offset-8 cursor-pointer" ref="dropdown-menu">
+            <div class="flex items-center" @click="toggleDropdown">
+              <div>Learn More</div>
+              <img
+                class="ms-4 transition-transform"
+                :class="{ 'rotate-180': isMenuOpen }"
+                src="/assets/svg/chevron-down.svg"
+              >
+            </div>
+            <div
+              class="absolute bg-white -ms-8 mt-2 h-0 overflow-hidden transition-all duration-300"
+              :class="{ 'h-40': isMenuOpen }"
+            >
+              <ul class="p-2">
+                <li class="p-2 mb-2 hover:underline hover:underline-offset-8">
+                  <NuxtLink :to="'/learn-more/extreme-heat'" @click="closeDropdown">Extreme Heat</NuxtLink>
+                </li>
+                <li class="p-2 mb-2 hover:underline hover:underline-offset-8">
+                  <NuxtLink :to="'/learn-more/rain-and-flooding'" @click="closeDropdown">Rain and Flooding</NuxtLink>
+                </li>
+                <li class="p-2 mb-2 hover:underline hover:underline-offset-8">
+                  <NuxtLink :to="'/learn-more/air-pollution'" @click="closeDropdown">Air Pollution</NuxtLink>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="p-2 mr-4 hover:underline hover:underline-offset-8">
+            <NuxtLink :to="'/about'">About</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style lang="scss">
-
 </style>
