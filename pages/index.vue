6df9f193b197d199f05da241b1baca4dfd2d3e6d
Brycen Ong <brycenong321@gmail.com>
2025-05-02 18:19:39 +0800

    added home page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,77 @@
 <template>
-  <div>
-    Index
-  </div>
+  <section class="flex flex-col items-center text-center px-37">
+    <div class="text-title mb-16">How does Climate Change affect my health?</div>
+    <div class="text-h2">Climate change is a global health issue...</div>
+    <div class="text-body">but how does it really affect me? What are the dangers I am facing today, and how can I prepare for it now?</div>
+    <div class="text-button mt-19">Scroll Down</div>
+    <div class="mb-17">Chevron Down</div>
+  </section>
+  <section class="w-full flex flex-col items-center px-37">
+    <div class="flex justify-between gap-20">
+      <div class="w-full">
+        <div class="text-h1">Temperatures in the Philippines have become dangerous</div>
+        <div class="text-body">Climate change is making extreme temperatures more frequent, and Metro Manila may one day become a hotspot for deadly heat. 
+          While hot waether may seem normal in a tropical country, spending too much time in the heat, doing strenuous activities in hot places, 
+          and not drinking enough water can lead to ilnesses like <span class="font-bold">heat stroke</span></div>
+      </div>
+      <div class="w-full">
+        <div class="text-title">51°C</div>
+        <div class="text-h2">Highest heat index recorded in the Philippines</div>
+        <div class="text-label">as of May 2021, Philstar</div>
+      </div>
+    </div>
+    <div class="mb-17">Chevron Down</div>
+  </section>
+  <section class="w-full px-37">
+    <div class="flex justify-between gap-20">
+      <div class="w-full">
+        <div class="text-h1">Contaminated water is a host for disease</div>
+        <div class="text-body">Climate change has muddied the lines between seasons, and the weather in the Philippines has become unpredictable. 
+          Going outside unprepared can expose you to unexpected rain and flooding. 
+          Flooded areas can be a breeding ground for bacteria and insects, and exposure to contaminated water can cause illnesses 
+          like <span class="font-bold">leptospirosis</span> or <span class="font-bold">dengue</span></div>
+      </div>
+      <div class="w-full">
+        <div class="text-title">195,603</div>
+        <div class="text-h2">Dengue cases in the Philippines</div>
+        <div class="text-label">as of December 2023, World Health Organization</div>
+      </div>
+    </div>
+    <div class="mb-17">Chevron Down</div>
+  </section>
+  <section class="w-full px-37">
+    <div class="flex justify-between gap-20">
+      <div class="w-full">
+        <div class="text-h1">Air pollution can cause heart and lung problems</div>
+        <div class="text-body">Air pollution in Philippine cities, particularly Metro Manila, has gotten so bad that it's been mistaken for volcanic smog. 
+          Inhaling smoke from vehicles and other air polluters can increase your risk for <span>cardiorespiratory diseases</span>, 
+          and it may evenaffect your body's response to the effects of other viruses like COVID-19.</div>
+      </div>
+      <div class="w-full">
+        <div class="text-title">7 million</div>
+        <div class="text-h2">Deaths caused by air pollution each year</div>
+        <div class="text-label">World Health Organization</div>
+      </div>
+    </div>
+    <div class="mb-17">Chevron Down</div>
+  </section>
+  <section class="flex flex-col items-center px-37">
+    <div class="text-title">What can I do now?</div>
+    <div class="flex justify-between gap-4">
+      <div class="w-full">
+        <div class="text-h2">Build your Climate Care Kit</div>
+        <div class="text-body">Preparedness starts with you. One step you can take to protect your health is by including
+          climate-ready items in your personal health kit. Take the first step by learning how to build your own climate care kit with us.
+        </div>
+        <button class="text-button">Build my Kit</button>
+      </div>
+      <div class="w-full">
+        <div class=text-h2>Find your Climate Personality</div>
+        <div class="text-body">Your lifestyle, daily routines, and beliefs all play a role in your risk for the effects of climate change. 
+          Take our quiz to discover your unique climate personality and what it means for your health.
+        </div>
+        <button class="text-button">Take the Quiz</button>
+      </div>
+    </div>
+  </section>
 </template>
