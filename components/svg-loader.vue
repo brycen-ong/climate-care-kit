38886920aac41a3971939aea5db267a895457cea
Brycen <brycenong321@gmail.com>
2025-05-06 02:09:02 +0800

    added svg-loader component

---
+++ components/svg-loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="icon"></component>
+</template>
+
+<script setup lang="ts">
+  import IconChevronX from '~/assets/icons/chevron-x.svg';
+  import IconChevronY from '~/assets/icons/chevron-y.svg';
+
+  type Icon = 'chevron-x' | 'chevron-y';
+
+  const props = defineProps({
+    icon: String as PropType<Icon>,
+  });
+
+  const icon = computed(() => {
+    switch(props.icon) {
+      case 'chevron-x': return IconChevronX
+      case 'chevron-y': return IconChevronY
+    }
+  })
+
+</script>
