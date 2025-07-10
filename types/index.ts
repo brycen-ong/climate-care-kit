7dfb5f8870ae5450d75abe601811a81c4ef041a9
Brycen <brycenong321@gmail.com>
2025-07-10 09:49:20 +0800

    moved types to index

---
+++ types/index.ts
@@ -0,0 +1,4 @@
+export type Icon = 'chevron-x' | 'chevron-y' | 'plus' | 'minus'
+export type Result = 'sunny' | 'chilly' | 'windy' | 'calm'
+export type Size = 'small' | 'medium' | 'large'
+export type Slug = 'extreme-heat' | 'rain-and-flooding' | 'air-pollution'
