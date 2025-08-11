13abdc3859efe8b1fb2f453c237845fc4a5c4722
Brycen <brycenong321@gmail.com>
2025-08-11 11:51:15 +0800

    updated about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    About
-  </div>
+  <section class="section relative bg-[url(/assets/images/about/about-bg-1.webp)] bg-size-[100%_100%]">
+    <div class="container items-start">
+      <div class="text-h1 max-w-165">Climate change affects more than the environment...</div>
+      <div class="text-body mt-8 max-w-139">In the Philippines, common depictions of climate change include natural disasters in far-off communities. While these disasters are devastating, we may forget that it also affects our day-to-day health as well.</div>
+      <div class="text-body mt-8 max-w-139">Climate change is an issue of public health. As early as now, we need to start preparing for the situations that will arise because of the changing climate.</div>
+      <div class="bg-white mt-8 rounded-2xl w-139 p-4">
+        <div class="text-label italic">Note: Climate Care Kit is a student-researched project, and is not intended to be taken as professional medical advice. Always consult your healthcare providers for any concerns about your health.</div>
+        <div class="text-label italic mt-5">Does any of the information seem inaccurate to you? Is there anything you think we should know? Email us at <a href="mailto:climatecarekit@gmail.com" class="text-primary">climatecarekit@gmail.com</a></div>
+      </div>
+    </div>
+  </section>
+  <section class="section relative bg-[url(/assets/images/about/about-bg-2.webp)] bg-size-[100%_100%] text-center text-white">
+    <div class="container">
+      <div class="text-h1 mb-3">What does climate change mean for...</div>
+      <div class="flex"></div>
+    </div>
+  </section>
+  <section class="section relative bg-[url(/assets/images/about/about-bg-3.webp)] bg-size-[100%_100%] text-center">
+
+  </section>
 </template>
